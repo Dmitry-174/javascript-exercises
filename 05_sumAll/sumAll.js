dfa70c9af46f66d6881cf00592294594841687f4
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-12 12:52:58 +0500

    Make Ex. 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR'
+    } else if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR'
+    }
 
+    let result = 0;
+
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
