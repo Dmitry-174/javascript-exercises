179a8b2e2d7194727d19dace2be584587c2cac63
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-17 19:04:26 +0500

    Make Ex. 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const newString = string.toLowerCase().replaceAll(/\W/g, '');
+    const reversedString = newString.split('').reverse().join('');
+    return newString === reversedString;
 };
 
 // Do not edit below this line
