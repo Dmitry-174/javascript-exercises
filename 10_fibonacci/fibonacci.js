8d3f5b82efa9d60263aa5509a218f7a6901bc532
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-17 19:24:33 +0500

    Make Ex. 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number <= 0) return 'OOPS';
+    if (number == 1) return 1;
+    let initialValue = 0;
+    let secondValue = 1;
+    let fib;
+    for (let i = 2; i <= number; i++) {
+        fib = initialValue + secondValue;
+        initialValue = secondValue;
+        secondValue = fib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
