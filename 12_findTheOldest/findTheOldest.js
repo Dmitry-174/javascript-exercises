7c54c6a9f0efdbe8c04ade23c4811e2da537fe3d
Dmitrii <dmitry.valiev174@gmail.com>
2022-03-17 19:42:24 +0500

    Make Ex. 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function insertThisYear(person) {
+    if (!person.yearOfDeath) {
+        person.yearOfDeath = (new Date).getFullYear();
+    }
+}
 
+const findTheOldest = function (array) {
+    const newArray = array.sort((a, b) => {
+        insertThisYear(a);
+        insertThisYear(b);
+        return (b.yearOfDeath - b.yearOfBirth) -
+            (a.yearOfDeath - a.yearOfBirth);
+    })
+    return newArray[0];
 };
 
 // Do not edit below this line
